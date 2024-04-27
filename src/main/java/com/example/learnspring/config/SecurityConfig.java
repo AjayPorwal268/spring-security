8240628f43c7493e9ff79521a72b698b84c691e3
Ajay Porwal <a.porwal12@gmail.com>
2024-04-27 19:18:38 +0530

    Implement spring security with custom authentication provider

--- src/main/java/com/example/learnspring/config/SecurityConfig.java
+++ src/main/java/com/example/learnspring/config/SecurityConfig.java
@@ -1,18 +1,36 @@
 package com.example.learnspring.config;
 
+import com.example.learnspring.config.security.filters.CustomAuthenticationFilter;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .build();
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
 }
--- src/main/java/com/example/learnspring/controllers/UserDetailsController.java
+++ src/main/java/com/example/learnspring/controllers/UserDetailsController.java
@@ -1,29 +1,14 @@
 package com.example.learnspring.controllers;
 
-import com.example.learnspring.httpbody.User;
-import com.example.learnspring.services.JpaUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class UserDetailsController {
 
-    @Autowired
-    private JpaUserDetailsService jpaUserDetailsService;
-    @GetMapping("/users")
-    public void getUserList() {
-        var u = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(u);
-        u.getAuthorities().forEach(a -> System.out.println(a.getAuthority()));
-        //return jpaUserDetailsService.getUserList();
-    }
-    @GetMapping("/login")
-    public String login() {
+
+    @GetMapping("/demo")
+    public String demo() {
         return "login successful";
     }
-
 }
