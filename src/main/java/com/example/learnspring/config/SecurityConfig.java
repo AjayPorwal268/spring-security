ccd6fe0a0cfd2024f22e0a5fc8f087ea4f00fde2
Ajay Porwal <a.porwal12@gmail.com>
2024-05-04 19:29:56 +0530

    spring-security-endpoint-authorization

--- src/main/java/com/example/learnspring/config/SecurityConfig.java
+++ src/main/java/com/example/learnspring/config/SecurityConfig.java
@@ -1,36 +1,63 @@
 package com.example.learnspring.config;
 
-import com.example.learnspring.config.security.filters.CustomAuthenticationFilter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.cglib.proxy.NoOp;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@AllArgsConstructor
 public class SecurityConfig {
 
-    private final CustomAuthenticationFilter customAuthenticationFilter;
-
+    @Value("${secret.key}")
+    private String key;
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().authenticated()
+
+        return http.httpBasic()
                 .and()
+                .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/test").hasAuthority("read")
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+               // .anyRequest().authenticated() //End Point Level Authorization
+//                .anyRequest().permitAll()
+//                .anyRequest().denyAll()
+//                .anyRequest().hasAnyAuthority("admin")
+//                .anyRequest().hasRole("admin")
+//                .anyRequest().hasAnyRole("ADMIN, "VISITOR")
                 .build();
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var uds = new InMemoryUserDetailsManager();
+
+        var u1 = User.withUsername("bill")
+                .password(passwordEncoder().encode("2345"))
+                .authorities("read").build();
+
+        var u2 = User.withUsername("john")
+                .password(passwordEncoder().encode("2345"))
+                .authorities("write").build();
+
+        uds.createUser(u1);
+        uds.createUser(u2);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- src/main/java/com/example/learnspring/controllers/UserDetailsController.java
+++ src/main/java/com/example/learnspring/controllers/UserDetailsController.java
@@ -1,6 +1,8 @@
 package com.example.learnspring.controllers;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,15 @@ public class UserDetailsController {
     @GetMapping("/demo")
     public String demo() {
         return "login successful";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test login successful";
+    }
+
+    @PostMapping("/test")
+    public String testPost() {
+        return "test post login successful";
     }
 }
